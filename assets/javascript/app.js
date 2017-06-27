28e434f7815da867dec90cddb6f0a3a522a1fe81
Cody Chase <cchase1991@gmail.com>
2017-06-27 17:45:12 -0400

    fix

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -197,16 +197,16 @@ var provider = new firebase.auth.GoogleAuthProvider();
 
 
 
-$("#abcRioButtonContentWrapper").on("click",function(event){
-    var provider = new firebase.auth.GoogleAuthProvider();
-    provider.addScope('profile');
-    provider.addScope('email');
-    firebase.auth().signInWithPopup(provider).then(function(result) {
- // This gives you a Google Access Token.
-    var token = result.credential.accessToken;
- // The signed-in user info.
-    var user = result.user;
-})
-
-		});
+//$("#abcRioButtonContentWrapper").on("click",function(event){
+//    var provider = new firebase.auth.GoogleAuthProvider();
+//    provider.addScope('profile');
+//    provider.addScope('email');
+//    firebase.auth().signInWithPopup(provider).then(function(result) {
+// // This gives you a Google Access Token.
+//    var token = result.credential.accessToken;
+// // The signed-in user info.
+//    var user = result.user;
+//});
+//
+//		});
 

40bc2645a850f6ea0f70fd0ec8505af182aba6ae
Cody Chase <cchase1991@gmail.com>
2017-06-27 17:06:52 -0400

    buttonfix

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -174,6 +174,32 @@ function addToMealPlan(){
    ev.target.appendChild(document.getElementById(data));
  }
 
+var provider = new firebase.auth.GoogleAuthProvider();
 
+//function signIn (){
+//    firebase.auth().signInWithPopup(provider).then(function(result) {
+//  // This gives you a Google Access Token. You can use it to access the Google API.
+//  var token = result.credential.accessToken;
+//  // The signed-in user info.
+//  var user = result.user;
+//  // ...
+//}).catch(function(error) {
+//  // Handle Errors here.
+//  var errorCode = error.code;
+//  var errorMessage = error.message;
+//  // The email of the user's account used.
+//  var email = error.email;
+//  // The firebase.auth.AuthCredential type that was used.
+//  var credential = error.credential;
+//  // ...
+//});
+//}
 
+$("#abcRioButtonContentWrapper").on("click",function(event){
+	var provider = new firebase.auth.GoogleAuthProvider();
+
+	firebase.auth().signInWithRedirect(provider)
+		.catch(function(error){
+			console.log("google sign in error", error);
+		});
 

a5f30dec16b81e7459469fa787ecce44b7cbc33e
Cody Chase <cchase1991@gmail.com>
2017-06-27 17:38:04 -0400

    btnfix

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -206,7 +206,7 @@ $("#abcRioButtonContentWrapper").on("click",function(event){
     var token = result.credential.accessToken;
  // The signed-in user info.
     var user = result.user;
-});
+})
 
 		});
 

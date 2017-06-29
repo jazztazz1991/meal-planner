823c28f729124d04e31a729bdab5e70ae9b52012
Cody Chase <cchase1991@gmail.com>
2017-06-29 12:41:22 -0400

    commented out maps

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -223,39 +223,39 @@ function signOut(){
     })
 }
 
-   var map;
-      var infowindow;
-      function initMap() {
-        var kirkman = {lat: 28.52188, lng: -81.4674207};
-        map = new google.maps.Map(document.getElementById('grocery-map'), {
-          center: kirkman,
-          zoom: 13
-        });
-        
-        infowindow = new google.maps.InfoWindow();
-        var service = new google.maps.places.PlacesService(map);
-        service.nearbySearch({
-          location: kirkman,
-          radius: 5000,
-          name: ['publix', 'walmart']
-        }, callback);
-      }
-      function callback(results, status) {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-          for (var i = 0; i < results.length; i++) {
-            createMarker(results[i]);
-          }
-        }
-      }
-      function createMarker(place) {
-        var placeLoc = place.geometry.location;
-        var marker = new google.maps.Marker({
-          map: map,
-          position: place.geometry.location
-        });
-        google.maps.event.addListener(marker, 'click', function() {
-          infowindow.setContent(place.name);
-          infowindow.open(map, this);
-        });
-      }
+//   var map;
+//      var infowindow;
+//      function initMap() {
+//        var kirkman = {lat: 28.52188, lng: -81.4674207};
+//        map = new google.maps.Map(document.getElementById('grocery-map'), {
+//          center: kirkman,
+//          zoom: 13
+//        });
+//        
+//        infowindow = new google.maps.InfoWindow();
+//        var service = new google.maps.places.PlacesService(map);
+//        service.nearbySearch({
+//          location: kirkman,
+//          radius: 5000,
+//          name: ['publix', 'walmart']
+//        }, callback);
+//      }
+//      function callback(results, status) {
+//        if (status === google.maps.places.PlacesServiceStatus.OK) {
+//          for (var i = 0; i < results.length; i++) {
+//            createMarker(results[i]);
+//          }
+//        }
+//      }
+//      function createMarker(place) {
+//        var placeLoc = place.geometry.location;
+//        var marker = new google.maps.Marker({
+//          map: map,
+//          position: place.geometry.location
+//        });
+//        google.maps.event.addListener(marker, 'click', function() {
+//          infowindow.setContent(place.name);
+//          infowindow.open(map, this);
+//        });
+//      }
 
